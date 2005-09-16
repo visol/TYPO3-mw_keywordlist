7dcd0f526e7d71a61829964d165c30af878ea122
a.beutel <Andreas.Beutel@mehrwert.de>
2005-09-16 10:13:00 +0000

    Uploaded ext to TER and synched EM_CONF

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "mw_keywordlist"
-#
-# Auto generated 15-09-2005 18:41
-#
+# 
+# Auto generated 16-09-2005 12:09
+# 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
 ########################################################################
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '1.3.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:11:{s:12:"ext_icon.gif";s:4:"b92c";s:17:"ext_localconf.php";s:4:"ccaf";s:15:"ext_php_api.dat";s:4:"4ef4";s:14:"ext_tables.php";s:4:"a7fa";s:28:"ext_typoscript_constants.txt";s:4:"572d";s:24:"ext_typoscript_setup.txt";s:4:"47ae";s:16:"locallang_db.php";s:4:"8a5e";s:14:"doc/manual.sxw";s:4:"ef3b";s:19:"doc/wizard_form.dat";s:4:"aa97";s:20:"doc/wizard_form.html";s:4:"24a4";s:34:"pi1/class.tx_mwkeywordlist_pi1.php";s:4:"edd8";}',
+	'version' => '1.3.1',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:11:{s:12:"ext_icon.gif";s:4:"b92c";s:17:"ext_localconf.php";s:4:"32e2";s:15:"ext_php_api.dat";s:4:"6276";s:14:"ext_tables.php";s:4:"90cd";s:28:"ext_typoscript_constants.txt";s:4:"572d";s:24:"ext_typoscript_setup.txt";s:4:"47ae";s:16:"locallang_db.php";s:4:"7323";s:14:"doc/manual.sxw";s:4:"ef3b";s:19:"doc/wizard_form.dat";s:4:"aa97";s:20:"doc/wizard_form.html";s:4:"24a4";s:34:"pi1/class.tx_mwkeywordlist_pi1.php";s:4:"7866";}',
 );
 
 ?>
