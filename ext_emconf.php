fdcdc6e1357a94b556c9a5a97980fb1a4714f4e5
mh <Michael.Huegelschaeffer@mehrwert.de>
2010-05-14 08:35:49 +0000

    Updated dependency settings

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "mw_keywordlist"
+# Extension Manager/Repository config file for ext "mw_keywordlist".
 #
-# Auto generated 03-05-2009 19:09
+# Auto generated 14-05-2010 10:34
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -35,14 +35,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '4.0.12-5.2.99',
-			'typo3' => '3.6.0-4.2.99',
+			'typo3' => '3.6.0-4.3.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:12:"ext_icon.gif";s:4:"b92c";s:17:"ext_localconf.php";s:4:"b932";s:15:"ext_php_api.dat";s:4:"6513";s:14:"ext_tables.php";s:4:"e356";s:28:"ext_typoscript_constants.txt";s:4:"c710";s:24:"ext_typoscript_setup.txt";s:4:"fd28";s:16:"locallang_db.php";s:4:"abb1";s:14:"doc/manual.sxw";s:4:"18e5";s:19:"doc/wizard_form.dat";s:4:"aa97";s:20:"doc/wizard_form.html";s:4:"24a4";s:34:"pi1/class.tx_mwkeywordlist_pi1.php";s:4:"ebd3";}',
+	'_md5_values_when_last_written' => 'a:11:{s:12:"ext_icon.gif";s:4:"b92c";s:17:"ext_localconf.php";s:4:"b932";s:15:"ext_php_api.dat";s:4:"80de";s:14:"ext_tables.php";s:4:"e356";s:28:"ext_typoscript_constants.txt";s:4:"c710";s:24:"ext_typoscript_setup.txt";s:4:"0504";s:16:"locallang_db.php";s:4:"abb1";s:14:"doc/manual.sxw";s:4:"18e5";s:19:"doc/wizard_form.dat";s:4:"aa97";s:20:"doc/wizard_form.html";s:4:"24a4";s:34:"pi1/class.tx_mwkeywordlist_pi1.php";s:4:"ae55";}',
 	'suggests' => array(
 	),
 );
