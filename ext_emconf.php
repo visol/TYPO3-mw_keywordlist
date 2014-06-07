f39a866422d40cf107ba370b93a5da47efa6f9ef
Andreas Beutel <Andreas.Beutel@mehrwert.de>
2014-06-07 09:04:48 +0200

    [CHANGE] Update constraints for PHP 5.4 and TYPO3 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.0.0-5.3.99',
-			'typo3' => '4.5.0-6.1.99',
+			'php' => '5.0.0-5.4.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
